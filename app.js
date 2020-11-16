8c5112e45e70dcff22be6defde9d5e03b029a5a3
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2020-11-16 17:28:46 -0300

    Final touches

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const inputEmail = document.querySelector('.input_email');
 const inputPassword = document.querySelector('.input_password');
 const inputConfirmPassword = document.querySelector('.input_confirm-password');
 const formBtn = document.querySelector('.form_btn');
+const completeMessageName = document.querySelector('.complete_message-name');
+const completeMessageEmail = document.querySelector('.complete_message-email');
+const completeMessagePassword = document.querySelector(
+  '.complete_message-password'
+);
+const completeMessageConfirmPassword = document.querySelector(
+  '.complete_message-confirm-password'
+);
 const alertMessageName = document.querySelector('.alert_message-name');
 const alertMessageEmail = document.querySelector('.alert_message-email');
 const alertMessagePassword = document.querySelector('.alert_message-password');
@@ -24,41 +32,51 @@ function inputValues() {
   var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
   if (valueName.length >= 6) {
-    successValid(inputName, alertMessageName);
+    successValid(inputName, alertMessageName, completeMessageName);
   } else {
-    failValid(inputName, alertMessageName);
+    failValid(inputName, alertMessageName, completeMessageName);
   }
 
   if (valueEmail.match(pattern)) {
-    successValid(inputEmail, alertMessageEmail);
+    successValid(inputEmail, alertMessageEmail, completeMessageEmail);
   } else {
-    failValid(inputEmail, alertMessageEmail);
+    failValid(inputEmail, alertMessageEmail, completeMessageEmail);
   }
 
   if (valuePassword.length >= 6) {
-    successValid(inputPassword, alertMessagePassword);
+    successValid(inputPassword, alertMessagePassword, completeMessagePassword);
   } else {
-    failValid(inputPassword, alertMessagePassword);
+    failValid(inputPassword, alertMessagePassword, completeMessagePassword);
   }
 
   if (
     valueConfirmPassword === valuePassword &&
     valueConfirmPassword.length >= 6
   ) {
-    successValid(inputConfirmPassword, alertMessageConfirmPassword);
+    successValid(
+      inputConfirmPassword,
+      alertMessageConfirmPassword,
+      completeMessageConfirmPassword
+    );
   } else {
-    failValid(inputConfirmPassword, alertMessageConfirmPassword);
+    failValid(
+      inputConfirmPassword,
+      alertMessageConfirmPassword,
+      completeMessageConfirmPassword
+    );
   }
 }
 
-function successValid(validation, message) {
+function successValid(validation, messageAlert, messageComplete) {
   validation.classList.add('success');
   validation.classList.remove('fail');
-  message.style.display = 'none';
+  messageAlert.style.display = 'none';
+  messageComplete.style.display = 'block';
 }
 
-function failValid(validation, message) {
+function failValid(validation, messageAlert, messageComplete) {
   validation.classList.add('fail');
   validation.classList.remove('success');
-  message.style.display = 'block';
+  messageAlert.style.display = 'block';
+  messageComplete.style.display = 'none';
 }
